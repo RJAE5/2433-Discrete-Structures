2ac2f7b01e2b956ea46f433e08f1c742ebe0863c
rjae5 <rykirjoe@yahoo.com>
2024-09-17 16:19:07 -0500

    Create main.cpp

---
+++ P01_Bitwise/main.cpp
@@ -0,0 +1,19 @@
+/*****************************************************************************
+*
+*  Author:           Rykir Evans
+*  Email:            rjevans0408@my.msutexas.edu
+*  Label:            P01
+*  Title:            Fraction Class
+*  Course:           CMPS 2433 Discrete Structures
+*  Professor:        Catherine Stringfellow
+*  Semester:         Fall 2024
+*
+*  Description:
+*         
+*
+*  Usage:
+*        
+*       
+*
+*  Files:             main.cpp
+*******************************************************************************/
